052e97b642ea03ad9b810a7d33399f0f376d61eb
Vikash <vikash.kumar6451@gmail.com>
2014-05-11 13:43:11 +0530

    Second Commit

--- Plot1.R
+++ Plot1.R
@@ -1,6 +1,6 @@
-mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
-myFile <- "C:\\Users\\VIKASH\\Documents\\household_power_consumption.txt"
-myData <- read.csv2.sql(myFile,mySql)
+mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'" # Create an SQL Query
+myFile <- "C:\\Users\\VIKASH\\Documents\\household_power_consumption.txt" # poin to the local file
+myData <- read.csv2.sql(myFile,mySql) # Read necessary line
 date_time<-paste(myData$Date,myData$Time)
 date_time<-strptime(date_time,"%d/%m/%Y %H:%M:%S")
 myData<-cbind(myData,date_time)
